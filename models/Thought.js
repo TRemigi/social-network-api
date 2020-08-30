ff60ecb24ff4ac2a17376fd1e2647c3fd953af4a
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-08-30 13:14:42 -0600

    set up file structure and create models

---
+++ models/Thought.js
@@ -0,0 +1,65 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema({
+    // set custom id to avoid confusion with parent comment _id
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+}, {
+    toJSON: {
+        getters: true
+    }
+});
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  });
+
+  // get total count of comments and replies on retrieval
+  ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+
+// create the Pizza model using the PizzaSchema
+const Thought = model('Thought', ThoughtSchema);
+
+// export the Pizza model
+module.exports = Thought;
