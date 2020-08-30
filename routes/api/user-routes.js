000a834a4327dc29b81378dec06e537303061c9e
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-08-30 13:27:41 -0600

    create user controllers and routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+  } = require('../../controllers/User-controller');
+
+// Set up GET all and POST at /api/users
+// /api/users
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createUser);
+
+// Set up GET one, PUT, and DELETE at /api/users/:id
+// /api/users/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
